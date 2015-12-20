29d0a6bcdd5013e032657bf136939d9e9ecf736f
Nik Butenko <nik@butenko.me>
2015-12-20 21:14:54 +1100

    Refactor VariableHeight example

--- src/example/VariableHeight.js
+++ src/example/VariableHeight.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import {shouldComponentUpdate} from 'react-addons-pure-render-mixin';
-import Collapse from '..';
+import Collapse from '../Collapse';
 import * as style from './style';
-
-const localStyle = height => ({
-  content: {
-    height,
-    background: 'rgba(96, 125, 139, 0.6)',
-    borderRadius: height / 2
-  }
-});
 
 
 const VariableHeight = React.createClass({
@@ -21,35 +13,32 @@ const VariableHeight = React.createClass({
   shouldComponentUpdate,
 
 
-  onChange({target: {value: height}}) {
-    this.setState({height});
-  },
-
-
-  onToggle() {
-    const {isOpened} = this.state;
-
-    this.setState({isOpened: !isOpened});
-  },
-
-
   render() {
     const {isOpened, height} = this.state;
 
     return (
       <div>
-
         <div style={style.config}>
-          <button onClick={this.onToggle}>{isOpened ? 'Close' : 'Open'}</button>
-          &nbsp;
-          Content height:
-          &nbsp;
-          <input type="range" step={50} min={0} max={500}
-            value={height} onChange={this.onChange} />
+          <label style={style.label}>
+            Opened:
+            <input style={style.input}
+              type="checkbox"
+              value={isOpened}
+              onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
+          </label>
+
+          <label style={style.label}>
+            Content height:
+            <input style={style.input}
+              type="range"
+              value={height} step={50} min={0} max={500}
+              onChange={({target: {value}}) => this.setState({height: value})} />
+            {height}
+          </label>
         </div>
 
         <Collapse isOpened={isOpened} style={style.container}>
-          <div style={{...localStyle(height).content, height}}></div>
+          <div style={{...style.getContent(height), height}}></div>
         </Collapse>
 
       </div>
