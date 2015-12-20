e3b26859ba9b47e6e38e722b343a2bb3f8166c93
Nik Butenko <nik@butenko.me>
2015-12-20 21:28:45 +1100

    Add keepContent to VariableHeight example

--- src/example/VariableHeight.js
+++ src/example/VariableHeight.js
@@ -6,7 +6,7 @@ import * as style from './style';
 
 const VariableHeight = React.createClass({
   getInitialState() {
-    return {isOpened: false, height: 100};
+    return {isOpened: false, keepContent: false, height: 100};
   },
 
 
@@ -14,7 +14,7 @@ const VariableHeight = React.createClass({
 
 
   render() {
-    const {isOpened, height} = this.state;
+    const {isOpened, keepContent, height} = this.state;
 
     return (
       <div>
@@ -23,8 +23,16 @@ const VariableHeight = React.createClass({
             Opened:
             <input style={style.input}
               type="checkbox"
-              value={isOpened}
+              checked={isOpened}
               onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
+          </label>
+
+          <label style={style.label}>
+            Keep content:
+            <input style={style.input}
+              type="checkbox"
+              checked={keepContent}
+              onChange={({target: {checked}}) => this.setState({keepContent: checked})} />
           </label>
 
           <label style={style.label}>
@@ -37,7 +45,10 @@ const VariableHeight = React.createClass({
           </label>
         </div>
 
-        <Collapse isOpened={isOpened} style={style.container}>
+        <Collapse
+          style={style.container}
+          isOpened={isOpened}
+          keepCollapsedContent={keepContent}>
           <div style={{...style.getContent(height), height}}></div>
         </Collapse>
 
