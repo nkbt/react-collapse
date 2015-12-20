bf77e28e4aa2cb79d8ff6167ff2b0a47f49c4804
Nik Butenko <nik@butenko.me>
2015-12-20 21:28:04 +1100

    Refactor FixedHeight example

--- src/example/FixedHeight.js
+++ src/example/FixedHeight.js
@@ -1,68 +1,66 @@
 import React from 'react';
 import {shouldComponentUpdate} from 'react-addons-pure-render-mixin';
-import Collapse from '..';
+import Collapse from '../Collapse';
 import * as style from './style';
-
-
-const localStyle = height => ({
-  content: {
-    height,
-    background: 'rgba(96, 125, 139, 0.6)',
-    borderRadius: height / 2
-  }
-});
 
 
 const FixedHeight = React.createClass({
   getInitialState() {
-    return {isOpened: false, content: 100, collapse: 200};
+    return {isOpened: false, keepContent: false, height: 100, fixedHeight: 200};
   },
 
 
   shouldComponentUpdate,
 
 
-  onChangeContent({target: {value}}) {
-    this.setState({content: parseInt(value, 10)});
-  },
-
-
-  onChangeCollapse({target: {value}}) {
-    this.setState({collapse: parseInt(value, 10)});
-  },
-
-
-  onToggle() {
-    const {isOpened} = this.state;
-
-    this.setState({isOpened: !isOpened});
-  },
-
-
   render() {
-    const {isOpened, collapse, content} = this.state;
+    const {isOpened, keepContent, height, fixedHeight} = this.state;
 
     return (
       <div>
-
         <div style={style.config}>
-          <button onClick={this.onToggle}>{isOpened ? 'Close' : 'Open'}</button>
-          &nbsp;
-          Content height:
-          &nbsp;
-          <input type="range" step={50} min={0} max={500}
-            value={content} onChange={this.onChangeContent} />
-          &nbsp;
-          Collapse fixed height:
-          &nbsp;
-          <input type="range" step={50} min={0} max={500}
-            value={collapse} onChange={this.onChangeCollapse} />
+          <label style={style.label}>
+            Opened:
+            <input style={style.input}
+              type="checkbox"
+              checked={isOpened}
+              onChange={({target: {checked}}) => this.setState({isOpened: checked})} />
+          </label>
+
+          <label style={style.label}>
+            Keep content:
+            <input style={style.input}
+              type="checkbox"
+              checked={keepContent}
+              onChange={({target: {checked}}) => this.setState({keepContent: checked})} />
+          </label>
+
+          <label style={style.label}>
+            Content height:
+            <input style={style.input}
+              type="range"
+              value={height} step={50} min={0} max={500}
+              onChange={({target: {value}}) => this.setState({height: value})} />
+            {height}
+          </label>
+
+          <label style={style.label}>
+            Collapse height:
+            <input style={style.input}
+              type="range"
+              value={fixedHeight} step={50} min={0} max={500}
+              onChange={({target: {value}}) => this.setState({fixedHeight: value})} />
+            {fixedHeight}
+          </label>
         </div>
 
-        <Collapse isOpened={isOpened} style={style.container} fixedHeight={collapse}>
-          <div style={{...localStyle(content).content, height: content}}></div>
+        <Collapse
+          style={style.container}
+          isOpened={isOpened}
+          fixedHeight={fixedHeight}
+          keepCollapsedContent={keepContent}>
+          <div style={{...style.getContent(height), height}}></div>
         </Collapse>
-
       </div>
     );
   }
