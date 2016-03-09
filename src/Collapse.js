62fa6aa341d56694bd55cab9b5e28b18be12bd7d
GeraldIstar <gerald.istar@gmail.com>
2016-03-09 16:40:39 +0600

    Fix bug #36
    https://github.com/nkbt/react-collapse/issues/36
    
    Update #36 bugfix
    
    Fix wrong state property assignment, remove variable with underscore
    
    Variable missing

--- src/Collapse.js
+++ src/Collapse.js
@@ -54,10 +54,16 @@ const Collapse = React.createClass({
   },
 
   onHeightReady(height) {
-    if (this.renderStatic && this.props.isOpened) {
+    const {isOpened, keepCollapsedContent} = this.props;
+
+    if (this.renderStatic && isOpened) {
       this.height = stringHeight(height);
     }
-    this.setState({height});
+    if (keepCollapsedContent) {
+      this.setState({height});
+    } else {
+      this.setState({height: isOpened ? height : 0});
+    }
 
     const reportHeight = this.props.isOpened ? height : 0;
 
@@ -161,7 +167,7 @@ const Collapse = React.createClass({
 
     return (
       <Motion
-        defaultStyle={{height: isOpened ? 0 : Math.max(0, height)}}
+        defaultStyle={{height: Math.max(0, height)}}
         style={{height: this.getMotionHeight(height)}}>
         {st => {
           this.height = stringHeight(st.height);
