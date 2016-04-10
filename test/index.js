c885f2bc3b7f437e549a1d675e9a25159d91ffe7
Nicklas Utgaard <nutgaard@gmail.com>
2016-04-10 17:40:43 +0200

    Fixed build issue
    The onHeightReady example manipulates window.scroll, and thus needs a make-shift DOM during test.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,8 @@
 import glob from 'glob';
 
+// Side-effect of scroll-example; Hooks.js
+global.window = {};
+global.navigator = {};
+global.document = {};
 
 glob.sync('**/*-test.js', {realpath: true, cwd: __dirname}).forEach(require);
