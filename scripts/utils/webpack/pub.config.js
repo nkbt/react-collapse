256a92b467a1dcebdce993ce3fa8cc32fdd3a5ac
Nik Butenko <nik@butenko.me>
2018-01-10 10:26:37 +1100

    Update build

--- scripts/utils/webpack/pub.config.js
+++ scripts/utils/webpack/pub.config.js
@@ -9,7 +9,8 @@ const {
   loaders,
   resolve,
   stats,
-  externals
+  externals,
+  INCLUDE_JS
 } = require(`./common`);
 
 
@@ -23,12 +24,7 @@ module.exports = {
   plugins: [
     plugins.define,
     plugins.html,
-    plugins.include([
-      `https://unpkg.com/react/dist/react.min.js`,
-      `https://unpkg.com/react-dom/dist/react-dom.min.js`,
-      `https://unpkg.com/react-motion/build/react-motion.js`,
-      `styles.css`
-    ]),
+    plugins.include(INCLUDE_JS.concat([`styles.css`])),
     new ExtractTextPlugin(`styles.css`)
   ],
   module: {
