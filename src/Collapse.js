24687c826e36d41e18bf566cd8f74ae6dacbb8b1
Nik Butenko <nik@butenko.me>
2018-04-10 14:39:41 +1000

    Complete rewrite

--- src/Collapse.js
+++ src/Collapse.js
@@ -1,229 +1,117 @@
-/* eslint-disable react/no-did-update-set-state,react/no-did-mount-set-state */
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Motion, spring} from 'react-motion';
-
-const SPRING_PRECISION = 1;
 
 
-const WAITING = 'WAITING';
-const RESIZING = 'RESIZING';
-const RESTING = 'RESTING';
-const IDLING = 'IDLING';
-
-
-const noop = () => null;
-const css = {
-  collapse: 'ReactCollapse--collapse',
-  content: 'ReactCollapse--content'
-};
-
-
-export class Collapse extends React.PureComponent {
+export class Collapse extends React.Component {
   static propTypes = {
+    theme: PropTypes.shape({
+      container: PropTypes.string,
+      content: PropTypes.string,
+      iframe: PropTypes.string
+    }),
     isOpened: PropTypes.bool.isRequired,
-    springConfig: PropTypes.objectOf(PropTypes.number),
-    forceInitialAnimation: PropTypes.bool,
-
-    hasNestedCollapse: PropTypes.bool,
-
-    fixedHeight: PropTypes.number,
-
-    theme: PropTypes.objectOf(PropTypes.string),
-    style: PropTypes.object,
-
-    onRender: PropTypes.func,
-    onRest: PropTypes.func,
-    onMeasure: PropTypes.func,
-
     children: PropTypes.node.isRequired
   };
 
 
   static defaultProps = {
-    springConfig: {},
-    forceInitialAnimation: false,
-    hasNestedCollapse: false,
-    fixedHeight: -1,
-    style: {},
-    theme: css,
-    onRender: noop,
-    onRest: noop,
-    onMeasure: noop
+    theme: PropTypes.shape({
+      container: 'ReactCollapse--container',
+      content: 'ReactCollapse--content',
+      sensor: 'ReactCollapse--sensor'
+    })
   };
 
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentState: IDLING,
-      from: 0,
-      to: 0
-    };
-  }
+  state = {height: 0};
 
 
   componentDidMount() {
-    const {isOpened, forceInitialAnimation, onRest} = this.props;
-    if (isOpened) {
-      const to = this.getTo();
-      if (forceInitialAnimation) {
-        const from = this.wrapper.clientHeight;
-        this.setState({currentState: RESIZING, from, to});
-      } else {
-        this.setState({currentState: IDLING, from: to, to});
-      }
-    }
-    onRest();
-  }
-
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.hasNestedCollapse) {
-      // For nested collapses we do not need to change to waiting state
-      // and should keep `height:auto`
-      // Because children will be animated and height will not jump anyway
-      // See https://github.com/nkbt/react-collapse/issues/76 for more details
-      if (nextProps.isOpened !== this.props.isOpened) {
-        // Still go to WAITING state if own isOpened was changed
-        this.setState({currentState: WAITING});
-      }
-    } else if (this.state.currentState === IDLING && (nextProps.isOpened || this.props.isOpened)) {
-      this.setState({currentState: WAITING});
+    if (this.sensor) {
+      this.sensor.contentWindow.addEventListener(`resize`, this.onResize, false);
+      this.raf = window.requestAnimationFrame(this.onResize);
     }
   }
 
 
-  componentDidUpdate(_, prevState) {
-    const {isOpened, onRest, onMeasure} = this.props;
+  shouldComponentUpdate(nextProps, nextState) {
+    const {theme, isOpened, children} = this.props;
+    const {height} = this.state;
 
-    if (this.state.currentState === IDLING) {
-      onRest();
-      return;
-    }
-
-    if (prevState.to !== this.state.to) {
-      onMeasure({height: this.state.to, width: this.content.clientWidth});
-    }
-
-    const from = this.wrapper.clientHeight;
-    const to = isOpened ? this.getTo() : 0;
-
-    if (from !== to) {
-      this.setState({currentState: RESIZING, from, to});
-      return;
-    }
-
-    if (this.state.currentState === RESTING || this.state.currentState === WAITING) {
-      this.setState({currentState: IDLING, from, to});
-    }
+    return children !== nextProps.children
+      || isOpened !== nextProps.isOpened
+      || height !== nextState.height
+      || Object.keys(theme).some(c => theme[c] !== nextProps.theme[c]);
   }
 
 
   componentWillUnmount() {
-    cancelAnimationFrame(this.raf);
+    if (this.raf !== null) {
+      window.cancelAnimationFrame(this.raf);
+      this.raf = null;
+    }
+    if (this.sensor) {
+      this.sensor.contentWindow.removeEventListener(`resize`, this.onResize, false);
+      this.sensor = null;
+    }
   }
 
 
-  onContentRef = content => {
-    this.content = content;
-  };
-
-
-  onWrapperRef = wrapper => {
-    this.wrapper = wrapper;
-  };
-
-
-  onRest = () => {
-    this.raf = requestAnimationFrame(this.setResting);
-  };
-
-
-  setResting = () => {
-    this.setState({currentState: RESTING});
-  };
-
-
-  getTo = () => {
-    const {fixedHeight} = this.props;
-    return (fixedHeight > -1) ? fixedHeight : this.content.clientHeight;
-  };
-
-
-  getWrapperStyle = height => {
-    if (this.state.currentState === IDLING && this.state.to) {
-      const {fixedHeight} = this.props;
-      if (fixedHeight > -1) {
-        return {overflow: 'hidden', height: fixedHeight};
-      }
-      return {height: 'auto'};
+  onResize = () => {
+    if (this.sensor) {
+      const {innerHeight} = this.sensor.contentWindow;
+      const height = Math.ceil(innerHeight);
+      this.setState({height});
     }
-
-    if (this.state.currentState === WAITING && !this.state.to) {
-      return {overflow: 'hidden', height: 0};
-    }
-
-    return {overflow: 'hidden', height: Math.max(0, height)};
   };
 
 
-  getMotionProps = () => {
-    const {springConfig} = this.props;
-
-    return this.state.currentState === IDLING ? {
-      // When completely stable, instantly jump to the position
-      defaultStyle: {height: this.state.to},
-      style: {height: this.state.to}
-    } : {
-      // Otherwise, animate
-      defaultStyle: {height: this.state.from},
-      style: {height: spring(this.state.to, {precision: SPRING_PRECISION, ...springConfig})}
-    };
+  onSensorRef = ref => {
+    this.sensor = ref;
   };
 
 
-  renderContent = ({height}) => { // eslint-disable-line
-    const {
-      isOpened: _isOpened,
-      springConfig: _springConfig,
-      forceInitialAnimation: _forceInitialAnimation,
-      hasNestedCollapse: _hasNestedCollapse,
-      fixedHeight: _fixedHeight,
-      theme,
-      style,
-      onRender,
-      onRest: _onRest,
-      onMeasure: _onMeasure,
-      children,
-      ...props
-    } = this.props;
-
-    const {
-      from,
-      to
-    } = this.state;
-
-    // DANGEROUS, use with caution, never do setState with it
-    onRender({current: height, from, to});
-
-    return (
-      <div
-        ref={this.onWrapperRef}
-        className={theme.collapse}
-        style={{...this.getWrapperStyle(Math.max(0, height)), ...style}}
-        {...props}>
-        <div ref={this.onContentRef} className={theme.content}>{children}</div>
-      </div>
-    );
-  };
+  raf = null;
+  sensor = null;
 
 
   render() {
+    const {isOpened, theme, children} = this.props;
+    const {height} = this.state;
+
     return (
-      <Motion {...this.getMotionProps()} onRest={this.onRest}>
-        {this.renderContent}
-      </Motion>
+      <div
+        style={{
+          height: isOpened ? height : 0,
+          overflow: 'hidden',
+          transition: 'height 200ms'
+        }}
+        className={theme.container}>
+        <div
+          style={{
+            position: 'relative'
+          }}
+          className={theme.content}>
+          <iframe
+            ref={this.onSensorRef}
+            title="Element resize sensor"
+            style={{
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              overflow: 'hidden',
+              border: 'none',
+              background: 'transparent',
+              pointerEvents: 'none',
+              zIndex: -1
+            }}
+            className={theme.sensor} />
+          {children}
+        </div>
+      </div>
     );
   }
 }
