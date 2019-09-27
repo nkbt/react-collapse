815d452d4fa8360114ea8ad8ba12e031f824ea90
Nik Butenko <nik@butenko.me>
2019-09-27 14:59:14 +0800

    Simplify build again

--- scripts/build.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-
-require('./lib');
-require('./dist');
--- scripts/lint.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-
-const {bash, CWD} = require('./utils/bash');
-
-
-const eslint = require.resolve('eslint/bin/eslint');
-
-bash(`${eslint} . ${process.argv.slice(2).join(' ')}`, {cwd: CWD});
