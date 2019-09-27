7e91aebd15730a4ef4c8d7df58bda6b5aa3017f9
Nik Butenko <nik@butenko.me>
2019-09-27 15:44:09 +0800

    Publish examples

--- bundle.js
+++ bundle.js
@@ -2836,15 +2836,15 @@ function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) r
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
@@ -2859,16 +2859,6 @@ var UnmountClosed =
 /*#__PURE__*/
 function (_React$PureComponent) {
   _inherits(UnmountClosed, _React$PureComponent);
-
-  _createClass(UnmountClosed, null, [{
-    key: "getDerivedStateFromProps",
-    value: function getDerivedStateFromProps(nextProps) {
-      return {
-        isResting: false,
-        isOpened: nextProps.isOpened
-      };
-    }
-  }]);
 
   function UnmountClosed(props) {
     var _this;
@@ -2944,6 +2934,20 @@ function (_React$PureComponent) {
   }
 
   _createClass(UnmountClosed, [{
+    key: "componentDidUpdate",
+    value: function componentDidUpdate(prevProps) {
+      var isOpened = this.props.isOpened;
+
+      if (prevProps.isOpened !== isOpened) {
+        // eslint-disable-next-line react/no-did-update-set-state
+        this.setState({
+          isResting: false,
+          isOpened: isOpened,
+          isInitialRender: false
+        });
+      }
+    }
+  }, {
     key: "render",
     value: function render() {
       var _this$state2 = this.state,
