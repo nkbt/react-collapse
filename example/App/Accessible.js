c09cc384926c288fba2855fd9f5e069fe94fcc53
Kyle Holmberg <kholmberg@acorns.com>
2020-02-15 19:52:07 -0800

    Create accessible examples

---
+++ example/App/Accessible.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import {Collapse} from '../../src';
+
+export function Accessible() {
+  const height = 100;
+
+  const accessibilityIds = {
+    checkbox: 'accessible-marker-example1',
+    button: 'accessible-marker-example2'
+  };
+
+  const [state, setState] = useState({
+    isCheckboxCollapseOpen: false,
+    isButtonCollapseOpen: false
+  });
+
+  return (
+    <div className="accessible">
+      <ul>
+        <li>
+          <div>
+            <h6>With a checkbox</h6>
+            <div className="config">
+              <label className="label">
+                Opened:
+                <input
+                  className="input"
+                  type="checkbox"
+                  aria-controls={accessibilityIds.checkbox}
+                  checked={state.isCheckboxCollapseOpen}
+                  onChange={({target: {checked}}) => setState({isCheckboxCollapseOpen: checked})} />
+              </label>
+            </div>
+            <Collapse
+              isOpened={state.isCheckboxCollapseOpen}
+              accessibilityId={accessibilityIds.checkbox}>
+              <div style={{height}} className="blob" />
+            </Collapse>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <h6>With a button</h6>
+            <div className="config">
+              <button
+                aria-controls={accessibilityIds.button}
+                aria-expanded={state.isButtonCollapseOpen}
+                onClick={() => setState({isButtonCollapseOpen: !state.isButtonCollapseOpen})}
+                type="button">
+                Reveal content
+              </button>
+            </div>
+            <Collapse
+              isOpened={state.isButtonCollapseOpen}
+              accessibilityId={accessibilityIds.button}>
+              <div style={{height}} className="blob" />
+            </Collapse>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
