03db8808c2dbe4f1d59a1dd0c239229a9c8b46c6
Nikita Butenko <nik@butenko.me>
2020-11-19 19:40:19 +1100

    Adjust eslint config harder

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     browser: true
   },
   rules: {
+    'operator-assignment': ['error', 'never'],
     'no-multiple-empty-lines': ['error', {max: 2}],
     'arrow-parens': ['error', 'as-needed'],
     'comma-dangle': ['error', 'never'],
